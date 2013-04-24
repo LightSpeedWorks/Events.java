8a0dfba6c9f7d12f1fc26f1e3b35f9572fbad20a
Kazuaki Nishizawa <Kazuaki.Nishizawa@gmail.com>
2013-04-25 00:50:41 +0900

    renewal test program

--- src/test/java/com/lightspeedworks/events/EventEmitterWorkMain.java
+++ src/test/java/com/lightspeedworks/events/EventEmitterWorkMain.java
@@ -1,5 +1,7 @@
 package com.lightspeedworks.events;
 
+import java.util.List;
+
 public class EventEmitterWorkMain {
 
 	/**
@@ -8,69 +10,81 @@ public class EventEmitterWorkMain {
 	public static void main(String[] args) {
 		EventEmitter emitter = new EventEmitter();
 
-		CB1 cb1 = new CB1();
-		CB2 cb2 = new CB2();
+		Listener1 listener1 = new Listener1();
+		Listener2 listener2 = new Listener2();
 
-		emitter.on("newListener", cb1);
-		emitter.on("test", cb2);
-		emitter.once("once", cb2);
+		emitter.on("newListener", listener1);
+		emitter.on("event1", listener2);
+		emitter.once("event2", listener2);
 
-		for (String event : emitter.events.keySet() ) {
+		for (String event : emitter.events.keySet()) {
 			System.out.print("event: " + event + ", ");
-			System.out.println("listeners: " + emitter.events.get(event));
+			System.out.print("listeners:");
+			List<EventHandler> handlers = emitter.events.get(event);
+			for (EventHandler handler : handlers)
+				System.out.print(" " + handler.callback + "." + handler.once);
+			System.out.println();
 		}
 
-		emitter.emit("test", "test case 1.1");
-		emitter.emit("test", "test case 1.2");
-		emitter.on("test", cb2);
-		emitter.emit("test", "test case 2");
-		emitter.removeListener("test", cb2);
-		emitter.emit("test", "test case 3");
-		emitter.removeListener("test", cb2);
-		emitter.emit("test", "test case 4");
+		emitter.emit("event1", "event1 case 1.1");
+		emitter.emit("event1", "event1 case 1.2");
+		emitter.on("event1", listener2);
+		emitter.emit("event1", "event1 case 2");
+		emitter.removeListener("event1", listener2);
+		emitter.emit("event1", "event1 case 3");
+		emitter.removeListener("event1", listener2);
+		emitter.emit("event1", "event1 case 4");
 
-		emitter.on("test", cb2);
-		emitter.on("test", cb2);
-		emitter.emit("test", "test case 5");
-		emitter.removeAllListeners("test");
-		emitter.emit("test", "test case 6");
+		emitter.on("event1", listener2);
+		emitter.on("event1", listener2);
+		emitter.emit("event1", "event1 case 5");
+		emitter.removeAllListeners("event1");
+		emitter.emit("event1", "event1 case 6");
 
-		emitter.emit("once", "once case 1.1");
-		emitter.emit("once", "once case 1.2");
+		emitter.emit("event2", "event2 case 1.1");
+		emitter.emit("event2", "event2 case 1.2");
 
-		emitter.on("test", cb2);
-		emitter.once("once", cb2);
+		emitter.on("event1", listener2);
+		emitter.once("event2", listener2);
 		emitter.removeAllListeners();
-		emitter.emit("test", "test case 7");
+		emitter.emit("event1", "event1 case 7");
 
-		emitter.once("error", cb2);
+		emitter.once("error", listener2);
 		emitter.emit("error", "error case 1.1");
-		emitter.emit("error", "error case 1.2");
+		// emitter.emit("error", "error case 1.2");
 
-		for (String event : emitter.events.keySet() ) {
+		for (String event : emitter.events.keySet()) {
 			System.out.print("event: " + event + ", ");
-			System.out.println("listeners: " + emitter.events.get(event));
+			System.out.print("listeners:");
+			List<EventHandler> handlers = emitter.events.get(event);
+			for (EventHandler handler : handlers)
+				System.out.print(" " + handler.callback + "." + handler.once);
+			System.out.println();
 		}
 		System.out.println("end");
 	}
 
-	static class CB1 implements Callback {
+	static class Listener1 implements Callback {
 		public void callback(Object... args) {
 			if (args.length < 1)
-				throw new Error("CB1 insufficient arguments");
+				throw new Error("Listener1 insufficient arguments");
 
-			String arg0 = (String) args[0];
-			System.out.println("CB1 callback: " + arg0);
+			System.out.print("Listener1 callback:");
+			for (Object arg : args)
+				System.out.print(" " + arg);
+			System.out.println();
 		}
 	}
 
-	static class CB2 implements Callback {
+	static class Listener2 implements Callback {
 		public void callback(Object... args) {
 			if (args.length < 1)
-				throw new Error("CB2 insufficient arguments");
+				throw new Error("Listener2 insufficient arguments");
 
-			String arg0 = (String) args[0];
-			System.out.println("CB2 callback: " + arg0);
+			System.out.print("Listener2 callback:");
+			for (Object arg : args)
+				System.out.print(" " + arg);
+			System.out.println();
 		}
 	}
 }
